8e997a335565aebb663bb2e8a26030f2c7809888
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-01-03 16:39:27 -0300

    feat: add findById method to EmployeeDAO

---
+++ src/main/java/repository/EmployeeDAO.java
@@ -0,0 +1,36 @@
+package repository;
+
+import com.mysql.cj.result.SqlDateValueFactory;
+import config.DatabaseConfig;
+import exception.customException.DatabaseException;
+import model.Employee;
+import model.Role;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Optional;
+
+public class EmployeeDAO {
+
+    public Optional<Employee> findById(Long id){
+        Employee employee = null;
+        String selectById = "SELECT * FROM employee WHERE id = ?";
+        try(Connection con = DatabaseConfig.getConnection() ; PreparedStatement preparedStatement = con.prepareStatement(selectById)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                employee = new Employee();
+                employee.setId(resultSet.getLong("id"));
+                employee.setName(resultSet.getString("name"));
+                employee.setSalary(resultSet.getBigDecimal("salary"));
+                employee.setRole(Role.valueOf(resultSet.getString("role")));
+                java.sql.Date dbDate = resultSet.getDate("hiring_date");
+                employee.setHiringDate(dbDate.toLocalDate());
+            }
+        } catch (Exception e) {
+            throw new DatabaseException("Error Selecting the employee");
+        }
+        return Optional.ofNullable(employee);
+    }
+}
