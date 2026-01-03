708781d920cd2e5dec7ec14cde261684a35e4f24
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-01-03 18:07:02 -0300

    feat: create delete method

--- src/main/java/repository/EmployeeDAO.java
+++ src/main/java/repository/EmployeeDAO.java
@@ -14,12 +14,12 @@ import java.util.Optional;
 
 public class EmployeeDAO {
 
-    public Optional<Employee> findById(Long id){
+    public Optional<Employee> findById(Long id) {
         Employee employee = null;
         String query = "SELECT * FROM employee WHERE id = ?;";
-        try(Connection con = DatabaseConfig.getConnection() ; PreparedStatement preparedStatement = con.prepareStatement(query)) {
+        try (Connection con = DatabaseConfig.getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query)) {
             preparedStatement.setLong(1, id);
-            try(ResultSet rs = preparedStatement.executeQuery()) {
+            try (ResultSet rs = preparedStatement.executeQuery()) {
                 if (rs.next()) {
                     employee = setDatabaseAttributesToEmployee(rs);
                 }
@@ -30,14 +30,14 @@ public class EmployeeDAO {
         return Optional.ofNullable(employee);
     }
 
-    public List<Employee> findAll(){
+    public List<Employee> findAll() {
         List<Employee> employeeList = new ArrayList<>();
         String query = "SELECT * FROM employee;";
-        try(Connection con = DatabaseConfig.getConnection() ;
-            PreparedStatement preparedStatement = con.prepareStatement(query);
-            ResultSet rs = preparedStatement.executeQuery();) {
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement preparedStatement = con.prepareStatement(query);
+             ResultSet rs = preparedStatement.executeQuery();) {
 
-            while (rs.next()){
+            while (rs.next()) {
                 employeeList.add(setDatabaseAttributesToEmployee(rs));
             }
         } catch (Exception e) {
@@ -46,43 +46,54 @@ public class EmployeeDAO {
         return employeeList;
     }
 
-    public Employee save(Employee employee){
+    public Employee save(Employee employee) {
         String query = "INSERT INTO employee (name,salary,role,hiring_date) VALUES (?,?,?,?);";
-        try(Connection con = DatabaseConfig.getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)){
+        try (Connection con = DatabaseConfig.getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
             setEmployeeStatements(preparedStatement, employee);
             preparedStatement.executeUpdate();
-            try(ResultSet rs = preparedStatement.getGeneratedKeys()){
-             if(rs.next()){
+            try (ResultSet rs = preparedStatement.getGeneratedKeys()) {
+                if (rs.next()) {
                     employee.setId(rs.getLong(1));
                 }
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new DatabaseException("Error on the insertion", e);
         }
         return employee;
     }
 
-    public Employee update(Employee employee){
+    public Employee update(Employee employee) {
         String query = "UPDATE employee SET name = ?, salary = ?, role = ?, hiring_date = ? WHERE id = ?;";
-        try(Connection con = DatabaseConfig.getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query)){
+        try (Connection con = DatabaseConfig.getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query)) {
             setEmployeeStatements(preparedStatement, employee);
             preparedStatement.setLong(5, employee.getId());
             int rowsAffected = preparedStatement.executeUpdate();
-            if(rowsAffected == 0){
-                throw new NotFoundException("The with ID: " + employee.getId() + " not found");
+            if (rowsAffected == 0) {
+                return null;
             }
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             throw new DatabaseException("Error on updating the employee", e);
         }
         return employee;
     }
 
+    public boolean delete(Long id) {
+        String query = "DELETE FROM employee WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            preparedStatement.setLong(1, id);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected == 0) {
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Error on deleting the employee", e);
+        }
+        return true;
+    }
 
 
     private void setEmployeeStatements(PreparedStatement preparedStatement, Employee employee) throws SQLException {
-        preparedStatement.setString(1,employee.getName());
+        preparedStatement.setString(1, employee.getName());
         preparedStatement.setBigDecimal(2, employee.getSalary());
         preparedStatement.setString(3, employee.getRole().name());
         preparedStatement.setDate(4, Date.valueOf(employee.getHiringDate()));
