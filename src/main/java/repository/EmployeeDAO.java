c5f815aa28e8f8f409afa26393f1f62295f2e0dd
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-01-03 16:53:59 -0300

    feat: add findAll method and helper method to set the database fields to employee

--- src/main/java/repository/EmployeeDAO.java
+++ src/main/java/repository/EmployeeDAO.java
@@ -9,6 +9,9 @@ import model.Role;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class EmployeeDAO {
@@ -20,17 +23,36 @@ public class EmployeeDAO {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             if(resultSet.next()){
-                employee = new Employee();
-                employee.setId(resultSet.getLong("id"));
-                employee.setName(resultSet.getString("name"));
-                employee.setSalary(resultSet.getBigDecimal("salary"));
-                employee.setRole(Role.valueOf(resultSet.getString("role")));
-                java.sql.Date dbDate = resultSet.getDate("hiring_date");
-                employee.setHiringDate(dbDate.toLocalDate());
+                employee = setDatabaseAttributesToEmployee(resultSet);
             }
         } catch (Exception e) {
-            throw new DatabaseException("Error Selecting the employee");
+            throw new DatabaseException("Error Selecting the employee", e);
         }
         return Optional.ofNullable(employee);
+    }
+
+    public List<Employee> findAll(){
+        List<Employee> employeeList = new ArrayList<>();
+        String selectAll = "SELECT * FROM employee";
+        try(Connection con = DatabaseConfig.getConnection() ; PreparedStatement preparedStatement = con.prepareStatement(selectAll)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                employeeList.add(setDatabaseAttributesToEmployee(resultSet));
+            }
+        } catch (Exception e) {
+            throw new DatabaseException("Error on Getting the employee list", e);
+        }
+        return employeeList;
+    }
+
+    private Employee setDatabaseAttributesToEmployee(ResultSet resultSet) throws SQLException {
+        Employee employee = new Employee();
+        employee.setId(resultSet.getLong("id"));
+        employee.setName(resultSet.getString("name"));
+        employee.setSalary(resultSet.getBigDecimal("salary"));
+        employee.setRole(Role.valueOf(resultSet.getString("role")));
+        java.sql.Date dbDate = resultSet.getDate("hiring_date");
+        employee.setHiringDate(dbDate.toLocalDate());
+        return employee;
     }
 }
