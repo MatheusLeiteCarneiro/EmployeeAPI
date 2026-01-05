e2d16781db6c9c9ca68d41e8b1f6dd5736d8f5e2
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-01-04 23:18:15 -0300

    feat: implemented FilterConfig class to automatically set the response to JSON format

---
+++ src/main/java/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+
+@WebFilter("/*")
+public class FilterConfig implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("application/json");
+        servletResponse.setCharacterEncoding("UTF-8");
+
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+}
