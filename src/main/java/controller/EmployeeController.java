4e014db17b370d358d4fe3fdf26a490bada885a8
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-01-04 22:58:44 -0300

    feat: implemented constructor with dependency injection, and configuration of the ObjectMapper to support LocalDate

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import repository.EmployeeDAO;
+import service.EmployeeService;
+
+import java.io.IOException;
+
+@WebServlet("/employees/*")
+public class EmployeeController extends HttpServlet {
+    private final EmployeeService service;
+    private final ObjectMapper objectMapper;
+
+    public EmployeeController() {
+        this.service = new EmployeeService(new EmployeeDAO());
+        this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
